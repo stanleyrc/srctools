048e2143fb6250a8d4d0b87557bbe6a9d33b689a
Stanley Clarke <sclarke@mskilab01.c.nygenome.org>
2024-02-19 10:55:05 -0500

    adds order to pgvdb templates

--- R/PGVdb_functions.R
+++ R/PGVdb_functions.R
@@ -12,48 +12,52 @@ getsharedPGV = function(web = "/gpfs/commons/home/sclarke/lab/pgv_content/", pgv
 }
 
 
-#get templates for different plots so I don't have to remember everything that the plots need
-bw_temp = function(patient_id = NA, x = list(NA), ref = NA, chart_type = "area", visible = TRUE, type = "bigwig", field = "foreground") {
+                                        #get templates for different plots so I don't have to remember everything that the plots need
+bw_temp = function(patient_id = NA,order = NA, x = list(NA), ref = NA, chart_type = "area", visible = TRUE, type = "bigwig", field = "foreground") {
     dt1 = data.table(patient.id = patient_id,
                      visible = visible,
                      x = x,
                      type = type,
                      field = field,
                      ref = ref,
+                     order = order,
                      defaultChartType = chart_type
                      )
     return(dt1)
 }
 
-arrow_temp = function(patient_id = NA, x = list(NA), ref = NA, chart_type = "scatterplot", visible = TRUE, type = "scatterplot", field = "foreground") {
+arrow_temp = function(patient_id = NA, order = NA, x = list(NA), ref = NA, chart_type = "scatterplot", visible = TRUE, type = "scatterplot", field = "foreground") {
     dt1 = data.table(patient.id = patient_id,
                      visible = visible,
                      x = x,
                      type = type,
                      field = field,
                      ref = ref,
+                     order = order,
                      defaultChartType = chart_type
                      )
     return(dt1)
 }
 
-genome_temp = function(patient_id = NA, x = list(NA), ref = NA, type = "genome", visible = TRUE) {
-    #use type = allelic to make a color a genome graph
+genome_temp = function(patient_id = NA, order = NA, x = list(NA), ref = NA, type = "genome", visible = TRUE) {
+                                        #use type = allelic to make a color a genome graph
     dt1 = data.table(patient.id = NA,
-                             type = type,
-                             visible = TRUE,
-                             x = NA,
-                             ref=ref
+                     type = type,
+                     visible = TRUE,
+                     x = NA,
+                     ref=ref,
+                     order = order,
                      )
 
     return(dt1)
 }
 
-walks_temp = function(patient_id = NA, x = list(NA), ref = NA, type = "genome", visible = TRUE) {
+walks_temp = function(patient_id = NA, order = NA, x = list(NA), ref = NA, type = "genome", visible = TRUE) {
     dt1 = data.table(patient.id = patient_id, 
-                                          visible = visible,
-                                          x = list(NA),
-                                          ref = ref
+                     visible = visible,
+                     x = list(NA),
+                     order = order,
+                     ref = ref
                      )
     return(dt1)
 }
